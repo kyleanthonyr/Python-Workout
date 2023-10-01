4e8c93c407925e0510ed19cb49cb05699a14bfc2
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2023-10-01 16:10:34 -0500

    edit: optimized code by using .append

--- Strings/pig_latin.py
+++ Strings/pig_latin.py
@@ -4,16 +4,16 @@ def pig_latin(sentence):
     Takes a single english word or sentence as input, and returns translated pig latin.
 
     """
-    translation = ''
+    translation = []
     for word in sentence.split():
         if word[0] in 'aeiou':
             translated_word = word + "way "
-            translation += translated_word
+            translation.append(translated_word)
         else:
             translated_word = word[1:] + word[0] + "ay "
-            translation += translated_word
+            translation.append(translated_word)
 
-    return translation
+    return ' '.join(translation)
 
 
 translation = pig_latin('what is pig latin')
